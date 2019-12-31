6df034a7c26b811baba8777717a496a86d846c8d
parshuramreddysudda <parshuram.sudda@gmail.com>
2019-12-31 23:11:02 +0530

    First Page Completed

--- materail-ui/src/App.js
+++ materail-ui/src/App.js
@@ -5,26 +5,64 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import RestoreIcon from '@material-ui/icons/Restore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
+import FolderIcon from '@material-ui/icons/Folder';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import {BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom';
 
-const style=makeStyles({
+const style=makeStyles(theme=>({
   root:{
     width:500,
-  }
-})
+    margin:'auto',
+    position: 'absolute',
+	top:0,
+	bottom: 0,
+	left: 0,
+	right: 0,
+  },
+  paper: {
+    padding: '16px',
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
 
 
+}));
+
+// class app extends React.Component{
+//   constructor(props) {
+//     super(props)
+  
+//     this.state = {
+//        heading:'Name Changes'
+//     };
+//   }
+  
+// }
 function Index() {
+ 
   const classes = style();
   const [value, setValue] = React.useState(0);
+
+  const handleChange=(event,newValue,value)=>{
+   console.log(value);
+    setValue(newValue);
+ };
+ 
   return (
 
     <div >
+ 
+    <Paper style={{marginTop:'2vh',marginBottom:'1vh'}}  className={classes.paper} ><h2 >This is the Navigation bar </h2></Paper>
+    <Paper className={classes.paper}><p>This includes Routing Configuration for Futher working</p></Paper>
+ 
+      <BottomNavigation value={value}  onChange={handleChange} showLabels className={classes.root}>
 
-      <BottomNavigation>
-         
 
-
-
+      <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} >as</BottomNavigationAction>
+      <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
+      <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
+      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
 
       </BottomNavigation>
         
---
+++ materail-ui/src/components/elements.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+import {makeStyles} from '@material-ui/core/styles';
+
+
+const usestyles= makeStyles(theme =>({
+ 
+    root: {
+        '& > *': {
+          margin: theme.spacing(1),
+        },
+      },
+ 
+
+}));
+export default function ContainedButtons() {
+    const classes = useStyles();
+  
+    return (
+      <div className={classes.root}>
+        <Button variant="contained">Default</Button>
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="contained" disabled>
+          Disabled
+        </Button>
+        <Button variant="contained" color="primary" href="#contained-buttons">
+          Link
+        </Button>
+      </div>
+    );
+  }
