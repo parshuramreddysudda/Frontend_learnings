d731aeb411b9801abf53f50e363e874355bf049e
parshuramreddysudda <parshuram.sudda@gmail.com>
2020-01-16 22:42:56 +0530

    Prism Configured

--- materail-ui/src/App.js
+++ materail-ui/src/App.js
@@ -1,92 +1,42 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
 import Navigation from '../src/components/Navigation';
 
-const style = makeStyles(theme => ({
-  root: {
-    // margin: 'auto',
-    color:'white',
-  },
-  paper: {
-    padding: '16px',
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-}));
 
+export default class PersonList extends React.Component {
+  state = {
+    name: '',
+  }
 
-function Index() {
+  handleChange = event => {
+    this.setState({ name: event.target.value });
+  }
 
-  const classes = style();
-  const [value, setValue] = React.useState(0);
+  handleSubmit = event => {
+    event.preventDefault();
+    const Appid = '6UT6K5-TKYR993W83';
+    const user = {
+      name: this.state.name
+    };
+    console.log(user.name);
 
-  const handleChange = (event, newValue, value) => {
-    console.log(value);
-    setValue(newValue);
-  };
+    axios.get(`http://127.0.0.1:4010/pets`)
+      .then(res => {console.log(res.data);
+      })
+  }
 
-  return (
-
-    <div >
-   <Navigation></Navigation>
-
-
-    <div className={classes.root}>
-    <Grid container spacing={3}>
-      <Grid item xs={12} md={12}>
-      <Paper className={classes.paper}>Full Col with XS 12 MD 12 </Paper>
-      </Grid>
-      <Grid item xs={12} sm={6}>
-      <Paper className={classes.paper}>Full Col with XS 12 SM 6</Paper>
-      </Grid>
-      <Grid item xs={12} sm={6}>
-      <Paper className={classes.paper}>Full Col with XS 12 SM 6</Paper>
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-      <Paper className={classes.paper}>Full Col with XS 12 SM 6 MD4</Paper>
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-      <Paper className={classes.paper}>Full Col with XS 12 SM 6 MD 4</Paper>
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-      <Paper className={classes.paper}>Full Col with XS 12 SM 6 MD4</Paper>
-      </Grid>
-  
-        <Grid item md={4} xs={6}>
-          <Paper className={classes.paper}>Md a-4 xs={4}</Paper>
-        </Grid>
-        <Grid item md={4} xs={6}>
-        <Paper className={classes.paper}>Md a-4 xs={4}</Paper>
-        </Grid>
-        <Grid item md={4} xs={6}>
-        <Paper className={classes.paper}>Md a-4 xs={4}</Paper>
-        </Grid>
-      
-      <Grid container spacing={3}>
-        <Grid item md={3} sm={6}  >
-          <Paper className={classes.paper}>Md a-4 xs={4}</Paper>
-        </Grid>
-        <Grid item md={3} sm={6}  >
-        <Paper className={classes.paper}>Md a-4 xs={4}</Paper>
-        </Grid>
-        <Grid item md={3} sm={6}  >
-        <Paper className={classes.paper}>Md a-4 xs={4}</Paper>
-        </Grid>
-        <Grid item md={3} sm={6}  >
-        <Paper className={classes.paper}>Md a-4 xs={4}</Paper>
-        </Grid>
-      </Grid>
-    </Grid>
-
-
-    </div>
-
-
-    </div>
-
-  );
+  render() {
+    return (
+      <div>
+        <Navigation></Navigation>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Person Name:
+            <input type="text" name="name" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    )
+  }
 }
-
-export default Index;
