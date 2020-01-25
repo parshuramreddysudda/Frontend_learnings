7359717d05895fab4f92a1237d516e118502f848
parshuramreddysudda <parshuram.sudda@gmail.com>
2020-01-25 21:20:22 +0530

    Prism Config Added

--- materail-ui/src/App.js
+++ materail-ui/src/App.js
@@ -3,73 +3,35 @@ import axios from 'axios';
 import Card from '../src/components/Card';
 import Navigation from '../src/components/Navigation';
 import Grid from '@material-ui/core/Grid';
- 
 
+ 
 export default class PersonList extends React.Component {
-  
+
   state = {
     name: '',
-    result:[],
-  }
-  handleChange = event => {
-    this.setState({ name: event.target.value });
+    result: [],
   }
 
-  handleSubmit = event => {
 
-    event.preventDefault();
-    const Appid = '6UT6K5-TKYR993W83';
-    const user = {
-      name: this.state.name
-    };
-    console.log(user.name);
-
-    axios.get(`https://newsapi.org/v2/everything?q=apple&from=2020-01-18&to=2020-01-18&sortBy=popularity&apiKey=66d4965851db4b4b9300f15d13443cfe`)
-      .then(res => {
-        // console.log(res.data.articles);
-        const result=res.data.articles;
-        this.setState({result});
-        // console.log(this.state.result);
-      })
-  }
-  
 
   render() {
-    
+
     return (
-      <div onLoad={this.MyApp}>
+
+      <div >
 
         <Navigation></Navigation>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Person Name:
-            <input type="text" name="name" onChange={this.handleChange} />
-          </label>
-          <button type="submit">Add</button>
-        </form>
-        <Grid container spacing={4}>
-      
-
-        { this.state.result.map((result,i) => 
-         <Grid item md={3} sm={6}  >
-       <Card id={i} author={result.author}  title={result.title} 
-         description={result.description} url={result.urlToImage} date={result.publishedAt} 
-         content={result.content}/>  
-              </Grid >
-  
-         )
          
-         }
-    
-       
-       </Grid>
-    
-  
-    
+        <Grid container spacing={4}>
 
-      
+
+        </Grid >
+ 
+
+
+
+
       </div>
     )
   }
 }
- 
