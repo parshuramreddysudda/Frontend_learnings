ca010104334b7b050602844fc60c4d4893100b80
parshuramreddysudda <parshuram.sudda@gmail.com>
2020-03-14 22:22:37 +0530

    Work Started

--- online-dashboard-react/src/App.js
+++ online-dashboard-react/src/App.js
@@ -1,26 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Paper from '@material-ui/core/Paper';
+import '../src/App.css';
+import Grid from '@material-ui/core/Grid';
+import Card from '../src/components/Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class Index extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+
+  }
+
+  render() {
+
+    const paper = {
+      padding: '1em',
+      textAlign: 'left',
+      fontStyle: 'bold',
+      fontWeight: '700',
+      fontSize: 'larger',
+    };
+
+    const smal = {
+      marginTop: '1em',
+      marginBottom: '1em',
+    }
+    const prize={
+      padding: "10px",
+      fontSize:'40px',
+    }
+
+    const main = {
+      padding: "1em",
+    }
+
+    return (
+
+      <div >
+        <Paper style={paper}>Online Retail Dashboard</Paper>
+        <Paper className='black' style={paper}>Summary </Paper>
+        <div className=' container section' style={smal}>
+          <div className='row'>
+
+            <div className='col-md-3'>
+
+              <Paper>
+                <div className='card-title' style={main}>
+                  Total Revenue
+                </div>
+                <div className='card-title' style={prize}>
+                   <icon style={{fontSize:'27px'}}>$</icon> asdas
+                </div>
+                
+              </Paper>
+              <div className='card'>
+
+                <div className='card-title'>
+                  asdas
+                  </div>
+                <p className='card-text'> asdasd</p>
+
+              </div>
+            </div>
+            <div className='col-md-3'>
+              asd
+              </div>
+            <div className='col-md-3'>
+              asd
+              </div>
+            <div className='col-md-3'>
+              asd
+              </div>
+
+
+          </div>
+
+        </div>
+      </div>
+    )
+  }
+
 }
-
-export default App;
