6a7f1e49c7688feb78cc1c56a1acf54723bd4a2e
parshuramreddysudda <parshuram.sudda@gmail.com>
2020-03-15 13:50:34 +0530

    Css ADDED

--- online-dashboard-react/src/App.js
+++ online-dashboard-react/src/App.js
@@ -5,6 +5,7 @@ import Paper from '@material-ui/core/Paper';
 import '../src/App.css';
 import Grid from '@material-ui/core/Grid';
 import Card from '../src/components/Card';
+import Circle from 'react-circle';
 
 
 
@@ -27,59 +28,119 @@ export default class Index extends React.Component {
     };
 
     const smal = {
-      marginTop: '1em',
-      marginBottom: '1em',
+      paddingBottom: '1em',
+      paddingTop: '1em',
     }
-    const prize={
+    const prize = {
       padding: "10px",
-      fontSize:'40px',
+      fontSize: '40px',
     }
 
     const main = {
       padding: "1em",
+      fontSize:"20px",
     }
 
     return (
 
-      <div >
+      <div className='darkBack' >
         <Paper style={paper}>Online Retail Dashboard</Paper>
-        <Paper className='black' style={paper}>Summary </Paper>
-        <div className=' container section' style={smal}>
+        <Paper className='darkBack' style={paper}>Summary </Paper>
+        <div className=' container-fluid pr-5 pl-5 section' style={smal}>
           <div className='row'>
-
-            <div className='col-md-3'>
-
-              <Paper>
-                <div className='card-title' style={main}>
+            <div className='col-md-3 col-lg-3'>
+              <Paper className="darkCard">
+                <div className='card-title cardTitleDark' style={main}>
                   Total Revenue
                 </div>
-                <div className='card-title' style={prize}>
-                   <icon style={{fontSize:'27px'}}>$</icon> asdas
+                <div className='card-text' style={prize}>
+                  <icon style={{ fontSize: '27px' }}>$</icon> asdas
                 </div>
-                
               </Paper>
-              <div className='card'>
-
-                <div className='card-title'>
-                  asdas
-                  </div>
-                <p className='card-text'> asdasd</p>
-
-              </div>
             </div>
             <div className='col-md-3'>
-              asd
-              </div>
+              <Paper className="darkCard">
+                <div className='card-title cardTitleDark' style={main}>
+                  Total Revenue
+                </div>
+                <div className='card-text' style={prize}>
+                  <icon style={{ fontSize: '27px' }}>$</icon> asdas
+                </div>
+              </Paper>
+            </div>
             <div className='col-md-3'>
-              asd
-              </div>
+              <Paper className="darkCard">
+                <div className='card-title cardTitleDark' style={main}>
+                  Total Revenue
+                </div>
+                <div className='card-text' style={prize}>
+                  <icon style={{ fontSize: '27px' }}>$</icon> asdas
+                </div>
+              </Paper>
+            </div>
             <div className='col-md-3'>
-              asd
+              <Paper className="darkCard">
+                <div className='card-title cardTitleDark' style={main}>
+                  Total Revenue
+                </div>
+                <div className='card-text' style={prize}>
+                  <icon style={{ fontSize: '27px' }}>$</icon> asdas
+                </div>
+              </Paper>
+            </div>
+          </div>
+
+        </div>
+
+        <div className=' container-fluid pr-5 pl-5 section' style={smal}>
+
+          <div className='row'>
+
+            <div className='col-md-3 col-lg-3 '>
+              <Paper className="darkCard fullHeight">
+                <div className='card-title cardTitleDark' style={main}>
+                  Total Revenue
+                </div>
+                <div className='card-text' style={prize}>
+                  <icon style={{ fontSize: '27px' }}>$</icon> asdas
+                </div>
+              </Paper>
+            </div>
+            <div className=' col-md-9 col-lg-9'>
+           
+<Paper className='darkCard' >
+<div className='row  text-center' style={smal}>
+                  <div className='col-sm-4'>
+                <div className='card-title cardTitleDark' style={main}>
+                  Purchase rate
+                </div>
+                <div className='card-text' style={prize}>
+                  <Circle animate={true} animationDuration="1s" lineWidth={50}  roundedStroke={true} textColor="white" bgColor="#161e29" progressColor="#386CBA" progress={85}></Circle>
+                </div>
               </div>
+              <div className='col-sm-4 borderLR'>
+                <div className='card-title cardTitleDark' style={main}>
+                 Checkout Price
+                </div>
+                <div className='card-text' style={prize}>
+                <Circle animate={true} animationDuration="1s" lineWidth={50}  roundedStroke={true} textColor="white" bgColor="#161e29" progressColor="#41B7C4" progress={35}></Circle>
+                </div>
+              </div>
+              <div className='col-sm-4'>
+                <div className='card-title cardTitleDark' style={main}>
+                Abandoned Cart Rate
+                </div>
+                <div className='card-text' style={prize}>
+                <Circle animate={true} animationDuration="1s" lineWidth={50}  roundedStroke={true} textColor="white" bgColor="#161e29" progressColor="#EDF9B0" progress={35}></Circle>
+                </div>
+              </div>
+              </div>
+              </Paper>
+              
+            </div>
 
 
           </div>
-
         </div>
       </div>
     )
