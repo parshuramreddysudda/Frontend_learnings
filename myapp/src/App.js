dfc3dcf5b1033c4592dd6d704b6c32ba2907ec37
parshuramreddysudda <parshuram.sudda@gmail.com>
2020-05-07 15:35:48 +0530

    Redux Configuration Started

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,82 +1,13 @@
-import React, { Component } from 'react';
-import ReactDom from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import logo, { ReactComponent } from './logo.svg';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import HelloWorld from './HelloWorld';
 
+class App extends Component {
+  state = {
+    tech: "Redux"
+  };
+  render() {
+    return <HelloWorld tech={this.state.tech} />;
+  }
+}
 
-
-
-// export default class PersonList extends React.Component {
-//   state = {
-//     persons: []
-//   }
-
-//   componentDidMount() {
-//     axios.get(`https://jsonplaceholder.typicode.com/users`)
-//       .then(res => {
-//         const persons = res.data;
-//         this.setState({ persons });
-//       })
-//   }
-
-//   render() {
-//     return (
-//       <ul>
-//         { this.state.persons.map(person => <li>{person.name}</li>)}
-//       </ul>
-//     )
-//   }
-// }
-
-// function Api() {
-//   return (
-  
-//      class App extends Component {
-//        constructor(){
-//          super();
-//        }
-//        componentWillMount(){
-//          this.getApi();
-//        }
-//        getApi(){
-//          var xr=new XMLHttpRequest();
-//          xr.addEventListener('load',() => {console.log(xr.response)}) ;
-//          xr.open('GET','https://dog.ceo/api/breeds/list/all');
-//          xr.send();
-//        }
-//      }
-//   );
-
-// }
-const apiElement=(
-  <div  className="card">
-  <h2 style={{color:'black'}}>This is the testing Boostrap File</h2>
-  <div className="row card">
-    <div className="col-md-4">
-
-    <img src={logo} className="App-logo" alt="This is the Logo Img"/>
-    </div>
-    <div className="col-md-4">
-    <img src={logo} className="App-logo" alt="This is the Logo Img"/>
-    </div>
-    <div className="col-md-4">
-    <img src={logo} className="App-logo" alt="This is the Logo Img"/>
-      </div>
-
-
-  </div>
-  </div>
-);
-ReactDom.render(apiElement,document.getElementById('test'));
-
- const testElement=(
-   <div>
-     <h2>This is the Main H3 component</h2>
-     <img src={logo} className="App-logo" alt="This is the Logo Img"/>
-   </div>
- );
-//  ReactDom.render(testElement,document.getElementById('test'));
-
-// export default App;
+export default App;
